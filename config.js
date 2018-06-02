4c51922600c82a7028e4b421fa5f21f316e16b24
Vaibhav Bhardwaj <bhardwajvbhv@gmail.com>
2018-06-02 18:40:16 +0530

    original Keys & Tokens removed

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  consumer_key:         'pCPJUSvnIzl9R3Ul0em8sesLM',
-  consumer_secret:      'ORd6LIw88IT7wYvTqBMEVOCU3T7am2mgaY3st7pOweJqUOJ0F5',
-  access_token:         '1002849521636761601-gbVXcnLSZte0orTCwtL3EItCM87jOW',
-  access_token_secret:  'L9wYjO3la8u6DavSamuyi1oJEJlxuOewtatSkvQuCOsM2',
+  consumer_key:         'enter your consumer_key here',
+  consumer_secret:      'enter your consumer_secret here',
+  access_token:         'enter your access_token here',
+  access_token_secret:  'enter your access_token_secret here',
   timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
   strictSSL:            true,     // optional - requires SSL certificates to be valid.
 }
